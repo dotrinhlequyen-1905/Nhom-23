16bb92f3de76d486139df8391feab934932b59f0
Đỗ Trịnh Lệ Quyên <160729512+dotrinhlequyen-1905@users.noreply.github.com>
2024-03-07 22:49:17 +0700

    Update and rename test.txt to Book.java

---
+++ src/main/java/com/mycompany/app/Week2/Code1/Book.java
@@ -0,0 +1,16 @@
+package com.mycompany.app.week2.code1;
+
+public class Book {
+    String title;
+    String author;
+    int numPages;
+    Book(String t, String a, int p) {
+        title = t;
+        author = Oda;
+        numPages = 50;
+    }
+    public static void main(String[] args) {
+        ;
+        
+    }
+}
