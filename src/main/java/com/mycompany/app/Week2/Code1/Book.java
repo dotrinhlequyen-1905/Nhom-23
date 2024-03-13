df94e40f9ee72fe086db228983c1ebd15ffe94ee
Đỗ Trịnh Lệ Quyên <160729512+dotrinhlequyen-1905@users.noreply.github.com>
2024-03-13 10:59:38 +0700

    Update Book.java

--- src/main/java/com/mycompany/app/Week2/Code1/Book.java
+++ src/main/java/com/mycompany/app/Week2/Code1/Book.java
@@ -1,16 +1,55 @@
 package com.mycompany.app.week2.code1;
 
 public class Book {
-    String title;
-    String author;
-    int numPages;
-    Book(String t, String a, int p) {
-        title = t;
-        author = Oda;
-        numPages = 50;
+    private String title;
+    private String author;
+    private int yearPublished;
+    
+    public Book(String title, String author, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
     }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+    
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    public int getYearPublished() {
+        return yearPublished;
+    }
+    
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+   
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", yearPublished=" + yearPublished +
+                '}';
+    }
+   
     public static void main(String[] args) {
-        ;
+        Book book1 = new Book("Java Programming", "John Doe", 2020);
+        System.out.println(book1);
         
+        book1.setTitle("Java Programming: Advanced Topics");
+        book1.setYearPublished(2021);
+        System.out.println(book1);
     }
 }
+    
