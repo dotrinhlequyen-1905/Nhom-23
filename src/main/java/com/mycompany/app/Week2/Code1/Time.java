f54100600a35f9ff9243c4bbe5a7b20c79311b37
Đỗ Trịnh Lệ Quyên <160729512+dotrinhlequyen-1905@users.noreply.github.com>
2024-03-13 10:43:19 +0700

    Create Time.java

---
+++ src/main/java/com/mycompany/app/Week2/Code1/Time.java
@@ -0,0 +1,65 @@
+public class Time {
+    private int hour;
+    private int minute;
+    private int second;
+    
+    public Time(int hour, int minute, int second) {
+        setTime(hour, minute, second);
+    }
+    
+    public void setHour(int hour) {
+        if (hour >= 0 && hour < 24) {
+            this.hour = hour;
+        } else {
+            throw new IllegalArgumentException("Hour must be between 0 and 23");
+        }
+    }
+    
+    public void setMinute(int minute) {
+        if (minute >= 0 && minute < 60) {
+            this.minute = minute;
+        } else {
+            throw new IllegalArgumentException("Minute must be between 0 and 59");
+        }
+    }
+    
+    public void setSecond(int second) {
+        if (second >= 0 && second < 60) {
+            this.second = second;
+        } else {
+            throw new IllegalArgumentException("Second must be between 0 and 59");
+        }
+    }
+ 
+    public int getHour() {
+        return hour;
+    }
+    
+    public int getMinute() {
+        return minute;
+    }
+    
+    public int getSecond() {
+        return second;
+    }
+    
+    public void setTime(int hour, int minute, int second) {
+        setHour(hour);
+        setMinute(minute);
+        setSecond(second);
+    }
+    
+    public String toString() {
+        return String.format("%02d:%02d:%02d", hour, minute, second);
+    }
+    
+    public static void main(String[] args) {
+        Time time = new Time(14, 30, 45);
+        System.out.println("Current time: " + time);
+        
+        time.setHour(18);
+        time.setMinute(15);
+        time.setSecond(0);
+        System.out.println("Modified time: " + time);
+    }
+}
