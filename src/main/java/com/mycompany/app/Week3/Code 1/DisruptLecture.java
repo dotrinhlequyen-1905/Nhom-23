a462044b985472e66a9d8c716be052e5df343fb7
Đỗ Trịnh Lệ Quyên <160729512+dotrinhlequyen-1905@users.noreply.github.com>
2024-03-17 22:48:27 +0700

    Update and rename test to DisruptLecture.java

---
+++ src/main/java/com/mycompany/app/Week3/Code 1/DisruptLecture.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class DisruptLecture {
+    public static void main(String[] args) {
+        String[] disruptions = {
+            "Screaming loudly!",
+            "Playing loud music!",
+            "Throwing confetti everywhere!",
+            "Releasing balloons!",
+            "Dancing wildly!",
+            "Blowing air horns!",
+            "Shining bright lights!"
+        };
+
+        Random random = new Random();
+
+        System.out.println("Starting the lecture disruption...");
+        for (int i = 0; i < disruptions.length; i++) {
+
+            int randomIndex = random.nextInt(disruptions.length);
+            String disruption = disruptions[randomIndex];
+
+            System.out.println("Disruption " + (i+1) + ": " + disruption);
+
+            try {
+                Thread.sleep(random.nextInt(3000) + 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Lecture disruption complete!");
+    }
+}
+
