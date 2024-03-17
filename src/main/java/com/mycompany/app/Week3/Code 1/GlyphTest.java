2c03a381732e6328dc972e14520b485dda0871fd
Đỗ Trịnh Lệ Quyên <160729512+dotrinhlequyen-1905@users.noreply.github.com>
2024-03-17 22:55:26 +0700

    Create GlyphTest.java

---
+++ src/main/java/com/mycompany/app/Week3/Code 1/GlyphTest.java
@@ -0,0 +1,27 @@
+package com.mycompany.app.week3.code1;
+class Glyph {
+    void draw() {
+        System.out.println("Glyph.draw()");
+    }
+}
+
+class RoundGlyph extends Glyph {
+    private int radius = 5;
+
+    RoundGlyph(int r) {
+        radius = r;
+        System.out.println("RoundGlyph.RoundGlyph(), radius = " + radius);
+    }
+
+    @Override
+    void draw() {
+        System.out.println("RoundGlyph.draw(), radius = " + radius);
+    }
+}
+
+public class GlyphTest {
+    public static void main(String[] args) {
+        Glyph glyph = new RoundGlyph(10);
+        glyph.draw();
+    }
+}
