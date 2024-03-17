c9f2912beeebd0ff84aa281bb6c8a803ac9ff084
Đỗ Trịnh Lệ Quyên <160729512+dotrinhlequyen-1905@users.noreply.github.com>
2024-03-17 22:51:24 +0700

    Create TestArithmetic.java

---
+++ src/main/java/com/mycompany/app/Week3/Code 1/TestArithmetic.java
@@ -0,0 +1,19 @@
+package com.mycompany.app.week3.code1;
+public class TestArithmetic {
+    public static void main(String[] args) {
+        int number1 = 20;
+        int number2 = 10;
+
+        int sum = number1 + number2;
+        int difference = number1 - number2;
+        int product = number1 * number2;
+        int quotient = number1 / number2;
+        int remainder = number1 % number2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
